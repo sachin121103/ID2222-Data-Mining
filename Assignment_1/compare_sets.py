7f8ba87c8f8fda6b0dd8ef510428cb8d5015833e
Sachin Prabhu Ram <sachin.prabhu.ram12@gmail.com>
2025-11-09 20:55:56 +0100

    Fixed bug in compare_sets

--- Assignment_1/compare_sets.py
+++ Assignment_1/compare_sets.py
@@ -1,11 +1,11 @@
-def compare_sets (set1, set2):
+def compare_sets(set1, set2):
 
     set_1 = set(set1)
     set_2 = set(set2)
     union = set_1.union(set_2)
     intersection = set_1.intersection(set_2)
 
-    if len(union):
+    if len(union) == 0:
         return 0.0
 
     jaccard_similarity = len(intersection)/len(union)
