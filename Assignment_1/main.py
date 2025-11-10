8c1a67fb751859ea58efc97591bf5de0344f16d8
Sachin Prabhu Ram <sachin.prabhu.ram12@gmail.com>
2025-11-10 23:26:14 +0100

    Updated data paths and sourcing

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -2,19 +2,18 @@ import shingling
 import min_hashing
 import compare_sets
 import compare_signatures
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-articles = {
-    "text_1": "the cat sat on the mat",
-    "text_2": "the cat sat on the mat", 
-    "text_3": "the cat sat on the rug",
-    "text_4": "the dog ran to me",
-    "text_5": "Completely unrelated sentence"
-}
+wars = pd.read_csv('data/middle_east_wars.csv', encoding='latin-1')
+
+iraq_wars = wars[wars['Keyword'] == 'Iraq']
+articles = iraq_wars['Headlines'].tolist()
 
 document_shingles = {}
 
-for id, text in articles.items():
+for id, text in enumerate(articles):
     shingles = shingling.shingler(text, k=5)
     document_shingles[id] = min_hashing.shingle_hasher(shingles)
     
@@ -28,14 +27,16 @@ for id, hashed_set in document_shingles.items():
 
 document_ids = list(signatures.keys())
 
+actual_similarities = []
+
 for i in range(len(document_ids)):
     for j in range(i+1, len(document_ids)):
         doc1, doc2 = document_ids[i], document_ids[j]
-
         actual_similarity = compare_sets.compare_sets(document_shingles[doc1], document_shingles[doc2])
-        estimated_similarity = compare_signatures.getSimilarity(signatures[doc1], signatures[doc2])
+        actual_similarities.append(actual_similarity)
 
-        print(f"{doc1} vs {doc2}")
-        print(f"  Actual: {actual_similarity:.3f}")
-        print(f"  Estimated: {estimated_similarity:.3f}")
-        print(f"  Error: {abs(actual_similarity - estimated_similarity):.3f}")
+plt.hist(actual_similarities, bins=50, edgecolor='black')
+plt.xlabel('Jaccard Similarity')
+plt.ylabel('Frequency')
+plt.title('Distribution of Headline Similarities in the Iraq War')
+plt.show()
