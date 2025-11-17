69f716f3936844ee271b556b22737cbea5bbd1ab
Anirudh Tiwari <anirudhtiwari2805@gmail.com>
2025-11-17 22:17:25 +0100

    added comments

--- Assignment_2/rule_generator.py
+++ Assignment_2/rule_generator.py
@@ -4,25 +4,25 @@ def generate_association_rules(all_frequent_itemsets, min_confidence):
    
     rules = []
 
-    for itemset, union_support_count in all_frequent_itemsets.items():
+    for itemset, union_support_count in all_frequent_itemsets.items(): # input set recieved from apriori
         
         k = len(itemset)
-        if k < 2:
+        if k < 2: #only look at doubletons or higher k-tons. cant make an implication from singletons
             continue
 
         items = list(itemset)
 
         for i in range(1, k):
-            for pair in combinations(items, i):
-                precursor = frozenset(pair)
-                consequent = itemset - precursor 
+            for pair in combinations(items, i): 
+                precursor = frozenset(pair) #generate all possible precursons (if part) with the lenght of k - 1
+                consequent = itemset - precursor #use set subtraction to find the consequent (then part) 
 
                 if precursor in all_frequent_itemsets:
-                    precursor_count = all_frequent_itemsets[precursor]
+                    precursor_count = all_frequent_itemsets[precursor] #count the occurences of precursor (if part)
                     
-                    confidence = union_support_count / precursor_count
+                    confidence = union_support_count / precursor_count#confidence computation
 
-                    if confidence >= min_confidence:
-                        rules.append((precursor, consequent, confidence))
+                    if confidence >= min_confidence: 
+                        rules.append((precursor, consequent, confidence)) #if passes min then appended to the rules set
 
     return rules
