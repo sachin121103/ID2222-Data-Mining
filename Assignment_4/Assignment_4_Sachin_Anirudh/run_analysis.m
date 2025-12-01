5c5b1f55c513950050a1534822cd277b72d0a910
Sachin Prabhu Ram <sachin.prabhu.ram12@gmail.com>
2025-12-01 20:00:31 +0100

    Removed comments

---
+++ Assignment_4/Assignment_4_Sachin_Anirudh/run_analysis.m
@@ -0,0 +1,41 @@
+clear; clc; close all;
+
+% --- 1. SETTINGS ---
+filename = 'example1.dat';  
+k = 3;                     
+sigma = 1.0;              
+
+% --- 2. LOAD DATA ---
+% Check if the file exists first
+if isfile(filename)
+    % Load data into a matrix
+    % 'load' works for space-separated .dat files
+    raw_data = load(filename);
+else
+    fprintf('File not found. Generating dummy concentric circles...\n');
+    theta = linspace(0,2*pi, 200)';
+end
+
+fprintf('Running Spectral Clustering...\n');
+[labels, Y_matrix, L_matrix] = spectral_clustering(raw_data, k, sigma);
+
+figure('Name', 'Spectral Clustering Results', 'Color', 'w');
+subplot(1, 2, 1);
+
+gscatter(raw_data(:,1), raw_data(:,2), labels);
+title('Final Clustering Result');
+xlabel('Feature 1'); ylabel('Feature 2');
+grid on; legend off;
+
+subplot(1, 2, 2);
+if k == 2
+    gscatter(Y_matrix(:,1), Y_matrix(:,2), labels);
+    title('Projected Space (Matrix Y)');
+    axis equal; % Make sure circles look like circles
+elseif k >= 3
+    scatter3(Y_matrix(:,1), Y_matrix(:,2), Y_matrix(:,3), 15, labels, 'filled');
+    title('Projected Space (Matrix Y)');
+end
+grid on;
+
+fprintf('Done! Check the plot window.\n');
---
+++ Assignment_4/Assignment_4_Sachin_Anirudh/specteral_clustering.m
@@ -0,0 +1,43 @@
+clear; clc; close all;
+
+% --- 1. SETTINGS ---
+filename = 'example1.dat';  
+k = 3;                     
+sigma = 1.0;              
+
+
+if isfile(filename)
+    raw_data = load(filename);
+else
+    fprintf('File not found. Generating dummy concentric circles...\n');
+    theta = linspace(0,2*pi, 200)';
+    c1 = [3*cos(theta), 3*sin(theta)] + 0.1*randn(200,2);
+    c2 = [6*cos(theta), 6*sin(theta)] + 0.1*randn(200,2);
+    raw_data = [c1; c2];
+    k = 2; 
+end
+
+fprintf('Running Spectral Clustering...\n');
+[labels, Y_matrix, L_matrix] = spectral_clustering(raw_data, k, sigma);
+
+figure('Name', 'Spectral Clustering Results', 'Color', 'w');
+
+subplot(1, 2, 1);
+gscatter(raw_data(:,1), raw_data(:,2), labels);
+title('Final Clustering Result');
+xlabel('Feature 1'); ylabel('Feature 2');
+grid on; legend off;
+
+
+subplot(1, 2, 2);
+if k == 2
+    gscatter(Y_matrix(:,1), Y_matrix(:,2), labels);
+    title('Projected Space (Matrix Y)');
+    axis equal; % Make sure circles look like circles
+elseif k >= 3
+    scatter3(Y_matrix(:,1), Y_matrix(:,2), Y_matrix(:,3), 15, labels, 'filled');
+    title('Projected Space (Matrix Y)');
+end
+grid on;
+
+fprintf('Done! Check the plot window.\n');
